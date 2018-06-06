52fbe53281b564ba127c671e2f0c1669f028f171
WojciechKapko <contact@madpixels.co.uk>
2018-06-06 22:26:28 +0200

    Changes

--- CarService/Data/ApplicationDbContext.cs
+++ CarService/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace CarService.Data
         {
         }
 
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
---
+++ CarService/Models/ServiceType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarService.Models
+{
+    public class ServiceType
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
